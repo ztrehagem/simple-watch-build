4bab35047e5bd834ae14502e5d3a5d09461438f0
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2022-06-11 18:33:28 +0900

    allow top-level await

--- build/pug.js
+++ build/pug.js
@@ -2,32 +2,30 @@ import * as path from "node:path";
 import { glob } from "./utils.js";
 import { PugTask } from "./tasks/pug.js";
 
-(async () => {
-  const srcDir = path.resolve("src");
-  const outDir = path.resolve("dist");
-  const entries = await glob(["**/*.pug", "!**/_*"], { cwd: srcDir });
+const srcDir = path.resolve("src");
+const outDir = path.resolve("dist");
+const entries = await glob(["**/*.pug", "!**/_*"], { cwd: srcDir });
 
-  const results = await Promise.allSettled(
-    entries.map(async (entry) => {
-      try {
-        const srcPath = path.resolve(srcDir, entry);
-        const outPath = path.resolve(outDir, entry).replace(/\.pug$/, ".html");
+const results = await Promise.allSettled(
+  entries.map(async (entry) => {
+    try {
+      const srcPath = path.resolve(srcDir, entry);
+      const outPath = path.resolve(outDir, entry).replace(/\.pug$/, ".html");
 
-        const task = new PugTask({ srcDir, srcPath, outPath });
-        await task.run();
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    }),
-  );
+      const task = new PugTask({ srcDir, srcPath, outPath });
+      await task.run();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }),
+);
 
-  const resolved = results.filter((result) => result.status == "fulfilled");
-  const rejected = results.filter((result) => result.status == "rejected");
+const resolved = results.filter((result) => result.status == "fulfilled");
+const rejected = results.filter((result) => result.status == "rejected");
 
-  console.log(`wrote: ${resolved.length}, error: ${rejected.length}`);
+console.log(`wrote: ${resolved.length}, error: ${rejected.length}`);
 
-  if (rejected.length) {
-    process.exit(1);
-  }
-})();
+if (rejected.length) {
+  process.exit(1);
+}
--- build/scss.js
+++ build/scss.js
@@ -2,32 +2,30 @@ import * as path from "node:path";
 import { glob } from "./utils.js";
 import { ScssTask } from "./tasks/scss.js";
 
-(async () => {
-  const srcDir = path.resolve("src");
-  const outDir = path.resolve("dist");
-  const entries = await glob(["**/*.scss", "!**/_*"], { cwd: srcDir });
+const srcDir = path.resolve("src");
+const outDir = path.resolve("dist");
+const entries = await glob(["**/*.scss", "!**/_*"], { cwd: srcDir });
 
-  const results = await Promise.allSettled(
-    entries.map(async (entry) => {
-      try {
-        const srcPath = path.resolve(srcDir, entry);
-        const outPath = path.resolve(outDir, entry).replace(/\.scss$/, ".css");
+const results = await Promise.allSettled(
+  entries.map(async (entry) => {
+    try {
+      const srcPath = path.resolve(srcDir, entry);
+      const outPath = path.resolve(outDir, entry).replace(/\.scss$/, ".css");
 
-        const task = new ScssTask({ srcDir, srcPath, outPath });
-        await task.run();
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    })
-  );
+      const task = new ScssTask({ srcDir, srcPath, outPath });
+      await task.run();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  })
+);
 
-  const resolved = results.filter((result) => result.status == "fulfilled");
-  const rejected = results.filter((result) => result.status == "rejected");
+const resolved = results.filter((result) => result.status == "fulfilled");
+const rejected = results.filter((result) => result.status == "rejected");
 
-  console.log(`wrote: ${resolved.length}, error: ${rejected.length}`);
+console.log(`wrote: ${resolved.length}, error: ${rejected.length}`);
 
-  if (rejected.length) {
-    process.exit(1);
-  }
-})();
+if (rejected.length) {
+  process.exit(1);
+}
